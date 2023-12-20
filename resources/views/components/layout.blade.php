5867743268f9432310202d7820b6e7596f10eba8
yamit cardenas <yamit.cardenas@canoeintelligence.com>
2023-12-20 15:47:15 -0500

    change menu according user logged

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -33,6 +33,21 @@
     ><img class="w-24 logo" src="{{asset('images/logo.png')}}" alt=""
         /></a>
     <ul class="flex space-x-6 mr-6 text-lg">
+
+        @auth
+            <li>
+                <span class="font-bold uppercase">
+                    Welcome {{auth()->user()->name}}
+                </span>
+            </li>
+            <li>
+                <a href="/listings/manage" class="hover:text-laravel"
+                ><i class="fa-solid fa-gear"></i>
+                    Manage Listings</a
+                >
+            </li>
+        @else
+
         <li>
             <a href="/register" class="hover:text-laravel"
             ><i class="fa-solid fa-user-plus"></i> Register</a
@@ -44,6 +59,7 @@
                 Login</a
             >
         </li>
+        @endauth
     </ul>
 </nav>
 <main>
